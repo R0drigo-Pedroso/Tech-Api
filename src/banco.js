0284d4505186c97bb5b8fcb9932e51af9b918956
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-09-01 11:22:06 -0300

    BD atualizado
    Conexão remota

--- src/banco.js
+++ src/banco.js
@@ -1,10 +1,18 @@
 import mysql2 from 'mysql2'
 
 const conexao = mysql2.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'escolinha'
+    // acesso remoto
+    host: 'ns204.hostgator.com.br',
+    user: 'aspect86_esc01',
+    password: 'W?yPWm21kKGN',
+    database: 'aspect86_escola01'
+
+
+    // Local - acesso
+    // host: 'localhost',
+    // user: 'root',
+    // password: '',
+    // database: 'escolinha'
 });
 
 // Conexão ao banco de dados
@@ -13,7 +21,7 @@ conexao.connect(erro => {
     if (erro){
         console.error(`Erro ao Conectar: ${erro.message}`);
     } else{
-        console.log(`Banco de dados conectado com sucesso!`);
+        console.log(`Banco de dados conectado em: ${conexao.config.host}`);
     }
 });
 
